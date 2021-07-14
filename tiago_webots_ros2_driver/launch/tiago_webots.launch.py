79f90b5e74a7957c05ac2960238c7f87ed82d581
rayvburn <chromedivizer@gmail.com>
2021-07-14 19:27:51 +0200

    driver pkg launch param remake, updated readme (main, driver), driver's package.xml cleanup

--- tiago_webots_ros2_driver/launch/tiago_webots.launch.py
+++ tiago_webots_ros2_driver/launch/tiago_webots.launch.py
@@ -15,26 +15,37 @@ from ament_index_python.packages import get_package_share_directory
 package_name = 'tiago_webots_ros2_driver'
 
 def generate_launch_description():
-    world_file = LaunchConfiguration('world_file', default='intralogistics.wbt')
+    world_dir = LaunchConfiguration(
+        'world',
+        default=os.path.join(
+            get_package_share_directory('tiago_webots_ros2_driver'),
+            'worlds',
+            'intralogistics.wbt'
+        )
+    )
 
     # Webots with TIAGo Iron driver
     webots = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('webots_ros2_core'), 'launch', 'robot_launch.py')
+            os.path.join(get_package_share_directory(
+                'webots_ros2_core'),
+                'launch',
+                'robot_launch.py'
+            )
         ),
         launch_arguments=[
             ('package', package_name),
             ('executable', 'tiago_driver'),
-            ('world', PathJoinSubstitution([get_package_share_directory(package_name), 'worlds', world_file])),
+            ('world', world_dir),
             ('output', 'screen')
         ]
     )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
-            name='world_file',
-            default_value=world_file,
-            description='name of the world file (including file extension)'
+            name='world',
+            default_value=world_dir,
+            description='directory of the world file (including file extension)'
         ),
         webots
     ])
